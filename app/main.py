952f7f2b3db04b9ac748bdf8089ce8bdb3a8c666
Jose David <j.caro0207@gmail.com>
2025-04-30 11:31:23 -0500

    [Add basic credentials for root endpoint ]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
-from fastapi import FastAPI, Request
+from typing import Annotated
+from fastapi import Depends, FastAPI, HTTPException, Request, status
 from datetime import datetime
 import zoneinfo
+
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from models import Customer, Transaction, Invoice
 from db import  create_all_tables
 from .routers import customers, transactions, invoices, plans
@@ -17,7 +20,7 @@ async def log_request_time(request: Request, call_next ):
     start_time = time.time()
     response = await call_next(request)
     process_time = time.time() - start_time
-    print(f'  -Request: {request.url}\n  -Headers: {request.headers} \n  -Completado en: {process_time:.4f} secs')
+    print(f'  -Request: {request.url}\n -Completado en: {process_time:.4f} secs')
     return response
      
 country_timezones = {
@@ -30,9 +33,14 @@ country_timezones = {
 
 db_customers: list[Customer] = []
 
+security = HTTPBasic()
+
 @app.get('/')
-async def root():
-    return {"message": "hola mundo"}
+async def root(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
+    print(credentials)
+    if credentials.username == 'holi' and credentials.password == 'holi': 
+        return {"message": f'Hola {credentials.username}'}
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
 
 @app.get('/time/{iso_code}')
 async def get_time_by_iso(iso_code:str):
@@ -41,3 +49,5 @@ async def get_time_by_iso(iso_code:str):
     tz=zoneinfo.ZoneInfo(timezone_str)
     print(tz)
     return {"time": datetime.now(tz)}
+
+
